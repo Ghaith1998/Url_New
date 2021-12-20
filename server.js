ac4089cb65dbd2ad4d43e7d7833f5c3862c41c0f
Ghaith_Ghanoum <57365042+Ghaith1998@users.noreply.github.com>
2021-12-20 22:19:51 +0100

    Fast fertig
    Fast fertig

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const res = require("express/lib/response");
 
 const app = express();
 
 
 app.set("view engine", "ejs")
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({extended : false}))
 app.listen(process.env.PORT || 5000)
+
+
+const Urlz = []
 
 app.get("/", (req,res) => {
     res.render("main")
@@ -13,6 +17,28 @@ app.get("/", (req,res) => {
 })
 
 app.post("/shortUrl", (req,res) => {
-    res.redirect(req.body.fullUrl)
+    const newUrlz = {
+        urlID : Urlz.length + 1,
+        urlFull : req.body.fullUrl,
+        urlShort : "Ftta131c"
+    }
+    Urlz.push(newUrlz)
+    res.send(`
+    <table>
+    <thead>
+        <tr>
+            <th>Full Url</th>
+            <th>Short Url</th>
+        </tr>
+    </thead>
+    <tbody>
+    <tr>
+    <td id="fl"><a href="${newUrlz.urlFull}">${newUrlz.urlFull}</a></td>
+    <td id="sh"><a href="${newUrlz.urlFull}">${newUrlz.urlShort}</a></td>
+</tr>
+    </tbody>
+</table>
+    `)
+
 })
 
